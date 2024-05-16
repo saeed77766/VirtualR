255e763bc71a5516897fd0ef7ce75ff2566d7b8e
saeed <saeed.babbar1861@gmail.com>
2024-05-17 02:52:00 +0500

    complete sectionHero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import HeroSection from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
   );
 };
