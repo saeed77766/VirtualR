f6be549ad62c91e47574532b50a754659b0f056c
saeed <saeed.babbar1861@gmail.com>
2024-05-18 18:19:26 +0500

    WorkFlow section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./Components/FeatureSection";
 import HeroSection from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
+import WorkFlow from "./Components/WorkFlow";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <WorkFlow />
       </div>
     </>
   );
